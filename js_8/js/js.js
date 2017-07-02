b8e550c9df28ea48db400eea4e1bcde67b08fd3c
chenxr0520 <402367310@qq.com>
2017-07-02 21:39:33 +0800

    javascriptDOM 8

--- js_8/js/js.js
+++ js_8/js/js.js
@@ -8,7 +8,7 @@ function dispalyAbbreviations(){ //获取和存放页面上所有缩略语的信
     var Alength = abbre.length;
     for(var i=0;i<Alength;i++){
         var current_abbr = abbre[i];
-        var definition = current_abbr.getAttribute("title"); //获取abbr标签上的title属性
+        var definition = current_abbr.getAttribute("title"); //获取abbr标签上的title属性的值，也就是文本节点的全称
         var key = current_abbr.lastChild.nodeType;   //   获得abbr里面的缩略语,也是就是元素中文本节点的值。
         defs[key]=definition;  //将信息已键值对的形式保存到数组中
     }
