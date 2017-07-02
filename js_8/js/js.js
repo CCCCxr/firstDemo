dcbb610c8413edf33c6c18100c178f94998f9ffb
chenxr0520 <402367310@qq.com>
2017-07-02 21:37:38 +0800

    javascriptDOM 8
    编写一个函数来存储页面上abbr属性的信息

---
+++ js_8/js/js.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+function dispalyAbbreviations(){ //获取和存放页面上所有缩略语的信息。
+    var abbre = document.getElementsByTagName("abbr");
+    if (abbre.length<1) return false;
+    var defs = [];  //定义一个数组来存放abbr的信息
+    var Alength = abbre.length;
+    for(var i=0;i<Alength;i++){
+        var current_abbr = abbre[i];
+        var definition = current_abbr.getAttribute("title"); //获取abbr标签上的title属性
+        var key = current_abbr.lastChild.nodeType;   //   获得abbr里面的缩略语,也是就是元素中文本节点的值。
+        defs[key]=definition;  //将信息已键值对的形式保存到数组中
+    }
+}
